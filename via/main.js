b0a18231fe0e3a3a6d9d500f9a7620edc91acf17
harapan1231 <senang.harapan@gmail.com>
2017-05-13 23:55:26 +0900

    Add createElement

--- via/main.js
+++ via/main.js
@@ -1,8 +1,21 @@
 import Inferno from "inferno";
+import Component from "inferno-component";
+import createElement from 'inferno-create-element';
 import h from "inferno-hyperscript";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+      return (
+          h("div", {class: "container"}, "Hello, world")
+      );
+  }
+}
+
 Inferno.render(
-  h("div", "Hello, world"),
+  createElement(App, {}),
   document.getElementById("app")
 )
 
