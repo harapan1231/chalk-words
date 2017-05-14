978c510092a558f7a12fa2b447ace00b574320e4
harapan1231 <senang.harapan@gmail.com>
2017-05-14 23:40:59 +0900

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,24 +5,25 @@ import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-  moduleName: "Chalk Words",
-  entry: "via/main.js",
-  dest: "www/app.js",
-  format: "iife",
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    nodeResolve({
-      jsnext: true,
-      browser: true
-    }),
-    buble({
-      // uncomment this to use `import createElement from 'inferno-create-element';` with jsx
-      // jsx: 'createElement',
-      objectAssign: 'Object.assign'
-    }),
-    uglify(),
-    commonjs()
-  ]
+    moduleName: "Chalk Words",
+    entry: "via/main.js",
+    dest: "www/app.js",
+    format: "iife",
+    plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        nodeResolve({
+            jsnext: true,
+            browser: true
+        }),
+        commonjs(),
+        buble({
+            // uncomment this to use `import createElement from 'inferno-create-element';` with jsx
+            // jsx: 'createElement',
+            objectAssign: 'Object.assign'
+        })
+        // uncomment to uglify
+        // uglify()
+    ]
 }
